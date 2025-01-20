deab0d654607098e911acf313e8d70cdf5d96a2d
jpploss <jpploss@edu.ufes.br>
2025-01-19 22:38:34 -0300

    lista de adjacencia v1 pronta

--- listAdj.h
+++ listAdj.h
@@ -3,4 +3,16 @@
 
 typedef struct _listAdj listAdj;
 
+/// @brief Cria uma lista de adjacências vazia.
+/// @return Lista de adjacência vazia.
+listAdj* criaListaAdj();
+
+/// @brief Preenche uma lista de adjacência vazia a partir de um arquivo de entrada, que deve estar no padrão estabelecido.
+/// @param arqEntrada arquivo de entrada padronizado
+/// @param lAdj lista a ser preenchida
+void preencheListaAdj(FILE* arqEntrada, listAdj* lAdj);
+
+void destroiListAdj(listAdj* lAdj);
+
+
 #endif
